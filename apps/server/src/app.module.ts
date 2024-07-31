e5b75f8737b3184df4a979fd6169679da59771e0
junwoo <cjw8809@gmail.com>
2024-08-01 02:30:21 +0900

    content - comment 관계 변경

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { sqliteDevOptions } from './infrastructure/adapter/typeorm/config/typeorm-dev-config';
+import { sqliteDevOptions } from './infrastructure/persistence/typeorm/config/typeorm-dev-config';
 
 @Module({
   imports: [TypeOrmModule.forRoot(sqliteDevOptions)],
