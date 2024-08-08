d6396e906665c927eae545c75d9a48bf9c5c57ee
junwoo <cjw8809@gmail.com>
2024-08-08 23:02:12 +0900

    jest base setup

---
+++ apps/server/jest.config.js
@@ -0,0 +1,17 @@
+/** @type {import('jest').Config} */
+
+const config = {
+  verbose: true,
+  moduleFileExtensions: ["js", "json", "ts"],
+  rootDir: "src",
+  testRegex: ".*\\.spec\\.ts$",
+  transform: {
+    "^.+\\.(t|j)s$": "ts-jest",
+  },
+  collectCoverageFrom: ["**/*.(t|j)s"],
+  coverageDirectory: "../coverage",
+  testEnvironment: "node",
+  setupFiles: ["dotenv/config"],
+};
+
+module.exports = config;
---
+++ apps/server/src/infrastructure/config/DatabaseConfig.spec.ts
@@ -0,0 +1,17 @@
+import { DatabaseConfig } from "./DatabaseConfig";
+
+describe("DatabaseConfig", () => {
+  describe("DB_FILE", () => {
+    it("should return the value of DB_FILE from environment variables", () => {
+      const result = DatabaseConfig.DB_FILE;
+      expect(result).toBe("test.sqlite");
+    });
+  });
+
+  describe("DB_LOG_ENABLE", () => {
+    it("should return the value of DB_LOG_ENABLE from environment variables", () => {
+      const result = DatabaseConfig.DB_LOG_ENABLE;
+      expect(result).toBe(true);
+    });
+  });
+});
