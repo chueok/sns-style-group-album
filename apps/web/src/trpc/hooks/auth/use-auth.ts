d6f463a3e0a6ec8c354a6da816fedcb7e9aadf6d
junu <cjw8809@gmail.com>
2025-05-24 17:22:37 +0900

    chore: 주석 추가

--- apps/web/src/trpc/hooks/auth/use-auth.ts
+++ apps/web/src/trpc/hooks/auth/use-auth.ts
@@ -3,6 +3,9 @@ import { trpc } from '@/trpc/trpc';
 export const useAuth = () => {
   const utils = trpc.useUtils();
 
+  // cookie 보안 정책으로 인해, cookie를 통해 로그인 여부 확인하기가 어려움
+  // 따라서 getMe를 통해 로그인 여부를 확인하고, 로그아웃 시 로그인 여부를 초기화함
+  // TODO: 이후 store를 통해 username, isLogin 등을 관리하면 변경 필요.
   const { data: user } = trpc.auth.getMe.useQuery(undefined, {
     gcTime: Infinity,
     staleTime: Infinity,
