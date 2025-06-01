a1efbd0ae46052db8bfd24938a01461efd4fcbe6
junu <cjw8809@gmail.com>
2025-06-02 00:49:13 +0900

    chore: encryption service 삭제

--- packages/be-core/src/domain/encryption/password-encryption-service.interface.ts
+++
@@ -1,5 +0,0 @@
-// NOTE : 현재 사용중이지 않음.
-export interface IPasswordEncryptionService {
-  hash(password: string): Promise<string>;
-  comparePassword(password: string, hashedPassword: string): Promise<boolean>;
-}
