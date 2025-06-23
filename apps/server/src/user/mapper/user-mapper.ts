cacacf4374d4eb55b6198d5e1929e97f0a3cfaa7
junu <cjw8809@gmail.com>
2025-06-24 01:23:10 +0900

    refactor: 사용하지 않는 user entity 정리

--- apps/server/src/user/mapper/user-mapper.ts
+++ apps/server/src/user/mapper/user-mapper.ts
@@ -1,48 +1,16 @@
-import {
-  SGroupProfile,
-  SSimpleGroupInfo,
-  TGroupProfile,
-  TSimpleGroupInfo,
-  TUser,
-} from '@repo/be-core';
+import { TUser } from '@repo/be-core';
 import { TypeormUser } from '../../infrastructure/persistence/typeorm/entity/user/typeorm-user.entity';
-
-export class GroupProfileMapper {
-  public static toDomainEntity(payload: TGroupProfile): TGroupProfile {
-    const result = SGroupProfile.parse(payload);
-    return result;
-  }
-}
-
-export class SimpleGroupInfoMapper {
-  public static toDomainEntity(payload: TSimpleGroupInfo): TSimpleGroupInfo {
-    const result = SSimpleGroupInfo.parse(payload);
-    return result;
-  }
-}
 
 export class UserMapper {
   public static toDomainEntity(payload: TypeormUser): TUser {
-    const {
-      id,
-      username,
-      email,
-      profileImageUrl,
-      createdDateTime,
-      updatedDateTime,
-      deletedDateTime,
-    } = payload;
+    const { id, username, email, profileImageUrl, createdDateTime } = payload;
 
     const user: TUser = {
       id,
       username: username || '',
       email,
       profileImageUrl,
-      groupProfiles: [], // TODO: 그룹 프로파일, invitedGroupList를 일반 TUser에서 리턴해야할지 검토 필요
-      invitedGroupList: [],
       createdDateTime,
-      updatedDateTime,
-      deletedDateTime,
     };
 
     return user;
